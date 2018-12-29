6ca39b03b29430b9a0347294d470fef31d16d215
ssyzyg <yaha.vip@qq.com>
2018-12-29 12:35:05 +0800

    Update xpath.py

--- lib/const/xpath.py
+++ lib/const/xpath.py
@@ -10,12 +10,13 @@ if SPIDER_NAME == LIANJIA_SPIDER:
     ERSHOUFANG_BANKUAI_XPATH = '//*[@id="filter-options"]/dl[1]/dd/div[2]/a'
     XIAOQU_QU_XPATH = '//*[@id="filter-options"]/dl[1]/dd/div/a'
     XIAOQU_BANKUAI_XPATH = '//*[@id="filter-options"]/dl[1]/dd/div[2]/a'
-    DISTRICT_AREA_XPATH = '//div[3]/div[1]/dl[2]/dd/div/div[2]/a'
-    CITY_DISTRICT_XPATH = '///div[3]/div[1]/dl[2]/dd/div/div/a'
+    DISTRICT_AREA_XPATH = '/html/body/div[3]/div[1]/dl[2]/dd/div/div[2]/a'
+    CITY_DISTRICT_XPATH = '/html/body/div[3]/div[1]/dl[2]/dd/div/div/a'
 elif SPIDER_NAME == BEIKE_SPIDER:
     ERSHOUFANG_QU_XPATH = '//*[@id="filter-options"]/dl[1]/dd/div/a'
     ERSHOUFANG_BANKUAI_XPATH = '//*[@id="filter-options"]/dl[1]/dd/div[2]/a'
     XIAOQU_QU_XPATH = '//*[@id="filter-options"]/dl[1]/dd/div/a'
     XIAOQU_BANKUAI_XPATH = '//*[@id="filter-options"]/dl[1]/dd/div[2]/a'
-    DISTRICT_AREA_XPATH = '//div[3]/div[1]/dl[2]/dd/div/div[2]/a'
-    CITY_DISTRICT_XPATH = '///div[3]/div[1]/dl[2]/dd/div/div/a'
+    DISTRICT_AREA_XPATH = '/html/body/div[3]/div[1]/dl[2]/dd/div/div[2]/a'
+    CITY_DISTRICT_XPATH = '/html/body/div[3]/div[1]/dl[2]/dd/div/div/a'
+
